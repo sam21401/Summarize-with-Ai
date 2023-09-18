1d69c673be869732e718569b63f0f57f6298d75a
sam21401 <samuelleonczyk@onet.eu>
2023-09-18 20:33:26 +0200

    Add content to Hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,32 @@
 import React from 'react'
 
+import {logo} from '../assets'
 export const Hero = () => {
   return (
-    <div>Hero</div>
+    <header className='w-full flex justify-center items-center flex-col'>
+        <nav className='flex justify-between items-center w-full mb-10 pt-3'>
+          <img src={logo} alt="sumz_logo" className='w-28 object-contain' />
+
+          <button 
+            type='button'
+            onClick={() => window.open('https://github.com/sam21401')}
+            className='black_btn'>
+
+              GitHub
+            </button>
+        </nav>
+
+        <h1 className='head_text'>
+          Summarize Articles with <br className='max-md:hidden'/>
+          <span className='green-accompaniment-gradient'>OpenAI GPT-4</span>
+        </h1>
+        <h2 className='desc'>
+          Get to the point, fast! 🚀<br />
+          Ditch the endless AI-generated scroll.<br /><br />
+          With Sumz, sift through the noise and find the gold. 🗑️✨
+        </h2>
+
+      </header>
   )
 }
+export default Hero
